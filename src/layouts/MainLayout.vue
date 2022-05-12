290e191d627c2cccb5b6bb484420d8ac3941384b
TheodorBeskow <92083686+TheodorBeskow@users.noreply.github.com>
2022-05-12 13:02:50 +0200

    bla

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,107 +1,108 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
+    <div class="bg-primary">
+      <q-header bordered class="bg-secondary text-primary row justify-center">
+        <q-tabs no-caps active-color="accent" indicator-color="transparent" class="text-primary" v-model="tab">
+          <q-tab name="Controller" label="Controller" class="underline" id="Controller"/>
+          <q-tab name="Panel" label="Panel" class="underline" id="Panel"/>
+          <q-tab name="Programs" label="Programs" class="underline" id="Programs"/>
+        </q-tabs>
+        <div class="text-primary absolute-right" style="top:5px; right: 5px">
+        <q-btn-toggle
+          v-model="model"
+          push
+          glossy
+          color="secondary"
+          toggle-color="accent"
+          :options="[
+            {label: 'Dark', value: 'Dark'},
+            {label: 'Light', value: 'Light'}
+          ]"
         />
+      </div>
+      </q-header>
+      
+      <!-- <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+        <q-list>
+          <q-item-label header> Essential Links </q-item-label>
 
-        <q-toolbar-title> Mqtt Chat </q-toolbar-title>
+          <EssentialLink
+            v-for="link in essentialLinks"
+            :key="link.title"
+            v-bind="link"
+          />
+        </q-list>
+      </q-drawer> -->
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+    </div>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from "components/EssentialLink.vue";
+// import EssentialLink from "components/EssentialLink.vue";
 
-const linksList = [
-  {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev",
-  },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework",
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev",
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev",
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev",
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev",
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev",
-  },
-];
 
-import { defineComponent, ref } from "vue";
 
-export default defineComponent({
+import { ref } from "vue";
+
+export default {
   name: "MainLayout",
-
-  components: {
-    EssentialLink,
-  },
-
   setup() {
-    const leftDrawerOpen = ref(false);
-
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer() {
-        leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
+      tab: ref('Controller'),
+      model: ref('Dark')
     };
   },
-});
+  watch: {
+    model: function(model) {
+      console.log(model);
+      // fixa senare
+    },
+    tab: function(tab) {
+      console.log(tab);
+      // var element1 = document.getElementById("Controller");
+      // var element2 = document.getElementById("Panel");
+      // var element3 = document.getElementById("Programs");
+      // element1.classList.remove("underline");
+      // element2.classList.remove("underline");
+      // element3.classList.remove("underline");
+      // if(tab == "Controller") element1.className += "underline";
+      // if(tab == "Panel") element2.className += "underline";
+      // if(tab == "Programs") element3.className += "underline";
+    }
+  }
+
+}
 </script>
+
+<style scoped>
+  .underline {
+  position: relative;
+  color: #000000;
+  text-decoration: none;
+}
+
+.underline:focus {
+  color: #000000;
+}
+.underline::before {
+  content: "";
+  position: absolute;
+  display: block;
+  width: 80%;
+  height: 2.5px;
+  bottom: 5px;
+  left: 10%;
+  background-color: #0aaab0;
+  transform: scaleX(0);
+  transition: transform 0.3s ease;
+}
+.underline:hover::before {
+  transform: scaleX(1);
+}
+</style>
+
+// https://vuejsexamples.com/tag/animation/
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,30 +1,7 @@
 <template>
-  <q-page class="bg-grey-3">
-    <div class="q-pa-md">
-      <div style="width: 100%">
-        <q-chat-message
-          v-for="info in chatMessages"
-          :name="info.user"
-          :avatar="
-            info.user == 'jeton'
-              ? 'https://cdn.quasar.dev/img/avatar1.jpg'
-              : 'https://cdn.quasar.dev/img/avatar2.jpg'
-          "
-          :text="[info.message]"
-          :sent="info.user == 'jeton'"
-          :bg-color="info.user == 'jeton' ? 'primary' : 'success'"
-        />
-      </div>
-    </div>
-    <div class="absolute-bottom row">
-      <q-input
-        type="text"
-        class="col bg-white"
-        v-model="publishMessage"
-        outlined
-      />
-      <q-btn @click="publish" icon="send" color="primary"></q-btn>
-    </div>
+  <q-page class="bg-primary" >
+    
+    
   </q-page>
 </template>
 
@@ -34,21 +11,22 @@ export default {
   created() {
     client.on("connect", () => {
       console.log("Conntected!");
-      client.subscribe("topic", function (err) {
+      client.subscribe("thebes", function (err) {
         if (!err) {
           let info = JSON.stringify({
-            user: "jeton",
+            user: "Theodor",
             message: "Hello mqtt",
           });
-          client.publish("topic", info);
+          client.publish("thebes", info);
+          // client.publish("thebes", info);
         }
       });
     });
 
     client.on("message", (topic, message) => {
-      console.log(`${topic} - ${message.toString()}`);
-      let info = JSON.parse(message.toString());
-      this.chatMessages.push(info);
+      // console.log(`${topic} - ${message.toString()}`);
+      // let info = JSON.parse(message.toString());
+      // // this.chatMessages.push(info);
     });
   },
   data() {
@@ -60,12 +38,11 @@ export default {
   },
   methods: {
     publish() {
-      let info = JSON.stringify({
-        user: "jeton",
-        message: this.publishMessage,
-      });
-      client.publish("topic", info);
-      this.publishMessage = "";
+      // for(let i = 0; i < 300; i++){
+      
+      client.publish("thebes", i.toString());
+      // }
+      
     },
   },
 };
